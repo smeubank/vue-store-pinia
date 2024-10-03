006f2b29ae2fd68bd322298241439caaa4c2eea4
Steven Eubank <eubank.steven88@gmail.com>
2024-10-03 20:48:17 +0200

    Update cart.js

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,23 +1,31 @@
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
-  state: () => ({
-    items: []
-  }),
+  state: () => {
+    console.log('Cart state created')
+    return {
+      items: []
+    }
+  },
   actions: {
     addItem(item) {
       const existingItem = this.items.find(i => i.id === item.id)
       if (existingItem) {
         existingItem.quantity += 1
+        console.log(`Increased quantity of ${item.name} to ${existingItem.quantity}`)
       } else {
         this.items.push({ ...item, quantity: 1 })
+        console.log(`Added ${item.name} to cart`)
       }
+      console.log('Current cart state:', JSON.stringify(this.items, null, 2))
     },
     removeItem(itemId) {
       const index = this.items.findIndex(item => item.id === itemId)
       if (index !== -1) {
+        console.log(`Removed ${this.items[index].name} from cart`)
         this.items.splice(index, 1)
       }
+      console.log('Current cart state:', JSON.stringify(this.items, null, 2))
     }
   },
   getters: {
