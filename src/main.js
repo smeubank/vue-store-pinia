a714eafc978344fe5205872ae6245d5bfa07430d
Steven Eubank <eubank.steven88@gmail.com>
2024-10-03 19:10:10 +0200

    add sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
+import * as Sentry from '@sentry/vue'
 import App from './App.vue'
 import LandingPage from './pages/LandingPage.vue'
 import ProductsPage from './pages/ProductsPage.vue'
@@ -20,6 +21,20 @@ const router = createRouter({
 const pinia = createPinia()
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: "https://4a85c87c7894458aff8578d0f2d2dd89@o673219.ingest.us.sentry.io/4508059881242624",
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
 app.use(pinia)
 app.use(router)
 app.mount('#app')
